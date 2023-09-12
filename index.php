8409ecf3033b95e1f7283aab6cc6390f0b4aa9b3
gunaseeli3 <gunaseeli@recruterundeveloppeur.com>
2023-09-12 11:24:29 +0530

    updated

--- index.php
+++ index.php
@@ -1,17 +1,8 @@
 <?php  
 ////// DESDE VISUAL A CERNET   DESCE CERNET a Visua test
 //LLAMADA AL ARCHIVO PRINCIPAL DE ACCESO A BASE DE DATOS E INTEGRACION DE SMARTY
-<<<<<<< HEAD
 error_reporting(0); //develop changes
 ob_start(); //ob start call
-=======
-error_reporting(0);//new changes
-<<<<<<< HEAD
-ob_start();
->>>>>>> ca9bc34 (updated index page)
-=======
-ob_start(); //ob start
->>>>>>> d8f672e (new changes)
 include("config.ini.php");
  
 $current_page = $_SERVER['REQUEST_URI'];
